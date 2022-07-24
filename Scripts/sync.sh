0ac02f577d0216771a8d845e0b8a6e7684314171
sukeeratsg <sukeerat@goindi.org>
2022-07-24 23:15:04 +0530

    Long live the king
    Signed-off-by: sukeeratsg <sukeerat@goindi.org>

--- Scripts/sync.sh
+++ Scripts/sync.sh
@@ -1,77 +1,15 @@
 #!/bin/bash
 
+set -e
+
 clear
-echo 'Please enter the device you wanna build for'
-read device
 
-rompath=$PWD
+git clone https://github.com/Havoc-Devices/android_device_xiaomi_raphael  device/xiaomi/raphael
+git clone https://github.com/Havoc-Devices/android_device_xiaomi_extras  device/xiaomi/extras --depth=1
+git clone https://github.com/Havoc-Devices/android_vendor_xiaomi_raphael --depth=1 
+git clone https://github.com/Havoc-Devices/kernel_xiaomi_raphael  kernel/xiaomi/raphael --depth=1 
+git fetch https://github.com/Havoc-Devices/hardware_qcom-caf_sm8150_display --depth=1 
+git clone  https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings --depth=1
+git clone https://gitlab.com/havoc-devices/trb_clang  prebuilts/clang/host/linux-x86/trb_clang -b 14 --depth=1
 
-cd ${rompath}/device
-rm -rf xiaomi asus
-
-cd ${rompath}/vendor
-rm -rf xiaomi asus
-
-cd ${rompath}/kernel 
-rm -rf xiaomi asus
-
-cd ${rompath}
-
-git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/proton-clang  --depth=1
-
-ln -s prebuilts/clang/host/linux-x86/proton-clang prebuilts/clang/host/linux-x86/clang-proton
-ln -s prebuilts/clang/host/linux-x86/proton-clang prebuilts/clang/host/linux-x86/proton
-
-if [ "$device" = "alioth" ];
-then
-
-git clone https://github.com/SukeeratSG/device_xiaomi_alioth device/xiaomi/alioth
-git clone https://github.com/SukeeratSG/device_xiaomi_sm8250-common device/xiaomi/sm8250-common
-git clone https://github.com/SukeeratSG/kernel_xiaomi_alioth kernel/xiaomi/alioth --depth=1
-git clone https://gitlab.com/Irongfly/vendor_xiaomi_alioth.git vendor/xiaomi/alioth --depth=1
-git clone https://github.com/SukeeratSG/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common --depth=1
-
-rm -rf hardware/qcom-caf/sm8250/display hardware/qcom-caf/sm8250/audio hardware/xiaomi
-
-git clone https://github.com/SukeeratSG/hardware_qcom-caf_sm8250_display  hardware/qcom-caf/sm8250/display
-git clone https://github.com/SukeeratSG/hardware_qcom-caf_sm8250_display  hardware/qcom-caf/sm8250/audio
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi 
-
-fi
-
-if [ "$device" = "raphael" ];
-then
-
-git clone https://github.com/SukeeratProgrammingServices/raphael device/xiaomi/raphael
-git clone https://github.com/SukeeratSG/android_device_xiaomi_extras  device/xiaomi/extras -b  arcana
-git clone https://github.com/SukeeratSG/raphael_kernel  kernel/xiaomi/raphael -b  arcana --depth=1
-git clone https://github.com/SukeeratSG/raphael_vendor vendor/xiaomi/raphael -b 12 --depth=1 
-
-git clone https://gitlab.com/varunhardgamer/trb_clang.git  prebuilts/clang/host/linux-x86/trb_clang -b 14 --depth=1
-
-cd device/xiaomi/raphael
-
-fi
-
-if [ "$device" = "X01BD" ];
-
-then
-
-git clone https://github.com/SukeeratSG/device_asus_X01BD  device/asus/X01BD
-git clone https://github.com/SukeeratSG/sdm660-common device/asus/sdm660-common  
-git clone https://github.com/SukeeratSG/vendor_asus  vendor/asus --depth=1
-git clone https://github.com:SukeeratSG/kernel_asus_sd660.git kernel/asus/sdm660 --depth=1
-gi
-
-fi
-
-if [ "$device" = "X00TD" ];
-
-then
-
-git clone https://github.com/SukeeratSG/device_asus_X00TD  device/asus/X00TD
-git clone https://github.com/SukeeratSG/sdm660-common device/asus/sdm660-common  
-git clone https://github.com/SukeeratSG/vendor_asus  vendor/asus --depth=1
-git clone https://github.com:SukeeratSG/kernel_asus_sd660.git kernel/asus/sdm660 --depth=1
-
-fi
+echo "Cloned Deps for raphael"
